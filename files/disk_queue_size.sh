7a41184c1f517abd179374ea239fb59fc83259c5
Yvonne Lam <yvonne@chef.io>
2017-07-04 21:49:55 -0700

    Extract test_id from the ec2 tag TestId. Collect data collector queue length and disk queue size.
    Signed-off-by: Yvonne Lam <yvonne@chef.io>

---
+++ files/disk_queue_size.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /home/centos/instance_info
+
+command=`iostat -x -d /dev/xvda | awk '/xvda/ { print $9}'`
+
+per_host_options="--namespace AWS/EC2 --region us-west-2 --dimensions TestId=$test_id,Instance=$instance"
+
+echo $command
+
+aws cloudwatch put-metric-data $per_host_options --metric-name ESDiskQueueSize --unit Count --value ${command}
