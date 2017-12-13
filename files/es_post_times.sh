56a3a77eeb1631c5c6739ee54cfb9870a9cde04f
Salim Afiune <afiune@chef.io>
2017-12-13 11:07:20 -0500

    Adds Gateway/Ingest/ES metrics to es_post_time.sh
    We might need to separate these metrics into different files.

--- files/es_post_times.sh
+++ files/es_post_times.sh
@@ -5,8 +5,10 @@ source /home/centos/instance_info
 
 per_host_options="--namespace AWS/EC2 --region us-west-2 --dimensions TestId=$test_id,Instance=$instance"
 
+LOGS_LAST_5MIN=$(journalctl --since "5 minutes ago" -u chef-automate)
+
 # Count HTTP codes
-data_collector_logs=`journalctl --since "5 minutes ago" | grep data-collector | grep POST`
+data_collector_logs=$(echo "$LOGS_LAST_5MIN" | grep data-collector | grep POST)
 COUNT_2XX=`echo "$data_collector_logs" | awk '{print $15}' | grep -c "^2"`
 COUNT_3XX=`echo "$data_collector_logs" | awk '{print $15}' | grep -c "^3"`
 COUNT_4XX=`echo "$data_collector_logs" | awk '{print $15}' | grep -c "^4"`
@@ -17,6 +19,18 @@ aws cloudwatch put-metric-data $per_host_options --metric-name 3xxStatuses --uni
 aws cloudwatch put-metric-data $per_host_options --metric-name 4xxStatuses --unit Count --value ${COUNT_4XX}
 aws cloudwatch put-metric-data $per_host_options --metric-name 5xxStatuses --unit Count --value ${COUNT_5XX}
 
-# Get average post times in seconds
-POST_REQUEST_TIME=`echo "$data_collector_logs" | awk '{gsub(/ms/, "")} {sum22+=$22} END {print (sum22/NR)/1000.0}'`
-aws cloudwatch put-metric-data $per_host_options --metric-name 5MinuteAveragePostRequestTime --unit Seconds --value ${POST_REQUEST_TIME}
+# Get average DataCollector POST times in seconds
+DC_POST_REQUEST_TIME=`echo "$data_collector_logs" | awk '{gsub(/ms/, "")} {sum22+=$22} END {print (sum22/NR)/1000.0}'`
+aws cloudwatch put-metric-data $per_host_options --metric-name 5MinuteAveragePostRequestTime --unit Seconds --value ${DC_POST_REQUEST_TIME}
+
+# Get the number of RPC Calls received through the legacy-endpoint (by the Gateway)
+GATEWAY_LEGACY_RPC_CALLS=$(echo "$LOGS_LAST_5MIN" | grep automate-gateway | grep "rpc call" | grep -c ProcessLegacyEvent)
+aws cloudwatch put-metric-data $per_host_options --metric-name LegacyEvents --unit Count --value ${GATEWAY_LEGACY_RPC_CALLS}
+
+# Get the number of ChefRun messages ingested (by the Ingest pipeline)
+INGESTED_MESSAGES=$(echo "$LOGS_LAST_5MIN" | grep -c "Chef run ingested successfully")
+aws cloudwatch put-metric-data $per_host_options --metric-name SuccessIngestedMessages --unit Count --value ${INGESTED_MESSAGES}
+
+# Average ES insertion time (How long does ES takes to insert documents?)
+ES_DOC_INSERT_TIME=$(echo "$LOGS_LAST_5MIN"| grep doc_insert| awk '{gsub(/ms/, "")} {print $(NF-1)}' |cut -d= -f2 | awk '{sum+=$1} END {print sum/NR}')
+aws cloudwatch put-metric-data $per_host_options --metric-name 5MinuteAverageESDocInsertTime --unit Milliseconds --value ${ES_DOC_INSERT_TIME}
